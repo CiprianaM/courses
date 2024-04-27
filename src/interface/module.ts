b6b82fdec8e3c4b2e46e9c7409376cf9f60c1638
Cipriana Milosanu <ciprianamilosanu@Ciprianas-MacBook-Pro.local>
2024-04-27 19:31:27 +0200

    add initial interface structure for entities

---
+++ src/interface/module.ts
@@ -0,0 +1,6 @@
+export interface Module {
+  id: number;
+  courseId: number;
+  duration: number;
+  position: number;
+}
---
+++ src/interface/userModuleProgress.ts
@@ -0,0 +1,6 @@
+export interface UserModuleProgress {
+  id: number;
+  moduleId: number;
+  userId: number;
+  status: string;
+}
